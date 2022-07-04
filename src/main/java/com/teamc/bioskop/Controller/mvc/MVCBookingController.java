f94a86c07c6d38a635a1e501620a851ec0736ca6
Yellia Viona <yelliaviona09@gmail.com>
2022-07-04 17:03:15 +0700

    add booking

--- src/main/java/com/teamc/bioskop/Controller/mvc/MVCBookingController.java
+++ src/main/java/com/teamc/bioskop/Controller/mvc/MVCBookingController.java
@@ -1,10 +1,13 @@
 package com.teamc.bioskop.Controller.mvc;
 
+import com.teamc.bioskop.Model.Booking;
 import com.teamc.bioskop.Service.BookingService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import javax.persistence.Id;
@@ -23,5 +26,21 @@ public class MVCBookingController {
         return "bookings";
     }
 
-//    @PostMapping("/update/{id}")
+    @GetMapping("/bookings/{id}")
+    public String showBookings(@PathVariable("id") Long id, Model model){
+        model.addAttribute("bookings", bookingService.getBookingById(id));
+        return "bookings";
+    }
+
+    @GetMapping("/add-bookings")
+    public String addBooking ( Model model){
+        Booking bookings = new Booking();
+        model.addAttribute("bookings", bookings);
+        return "add-booking";
+    }
+
+    @PostMapping("/tambah-bookings")
+    public String submitNewBooking(@ModelAttribute("bookings") Booking booking){
+        return"success";
+    }
 }
