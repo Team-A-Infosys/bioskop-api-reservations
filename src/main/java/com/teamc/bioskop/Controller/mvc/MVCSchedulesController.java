34591abd4d4fc203a5ad01c65c8136e2679d6277
Harist Islami <islamiharist1998@gmail.com>
2022-07-05 15:12:53 +0700

    Delete duplicating file

--- src/main/java/com/teamc/bioskop/Controller/mvc/MVCSchedulesController.java
+++ src/main/java/com/teamc/bioskop/Controller/mvc/MVCSchedulesController.java
@@ -1,4 +1,4 @@
-package com.teamc.bioskop.Controller.mvc;
+package com.teamc.bioskop.Controller.MVC;
 import com.teamc.bioskop.Model.Schedule;
 import com.teamc.bioskop.Service.ScheduleService;
 import lombok.AllArgsConstructor;
