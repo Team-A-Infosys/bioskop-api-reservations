ddc190112234de84c70455434e48abae5585bf27
Yellia Viona <yelliaviona09@gmail.com>
2022-07-05 11:31:02 +0700

    create,read,update udah oke, delete belum

--- src/main/java/com/teamc/bioskop/Controller/mvc/MVCBookingController.java
+++ src/main/java/com/teamc/bioskop/Controller/mvc/MVCBookingController.java
@@ -1,19 +1,22 @@
 package com.teamc.bioskop.Controller.mvc;
 
+import com.teamc.bioskop.Exception.ResourceNotFoundException;
 import com.teamc.bioskop.Model.Booking;
 import com.teamc.bioskop.Service.BookingService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import javax.persistence.Id;
 
 @AllArgsConstructor
 @Controller
+
 
 public class MVCBookingController {
 
@@ -42,7 +45,27 @@ public class MVCBookingController {
     @PostMapping("/tambah-bookings")
     public String submitNewBooking(@ModelAttribute("bookings") Booking booking){
         this.bookingService.createBooking(booking);
-
         return"success";
+    }
+
+    @GetMapping("/update/bookings/{id}")
+    public String showEditBooking (@PathVariable("id") Long id, Model model){
+        Booking bookings = this.bookingService.getReferenceById(id);
+        model.addAttribute("bookings", bookings);
+        return "update-booking";
+    }
+
+    @PostMapping("/updated/bookings/{id}")
+    public String updateById(@PathVariable("id") Long id,
+                             @ModelAttribute("bookings") Booking bookings){
+        bookings.setBookingId(id);
+        this.bookingService.updateBooking(bookings);
+        return "success-updated-booking";
+    }
+
+    @DeleteMapping("/delete/bookings{id}")
+    public String deleteById(Model model, @PathVariable("id") Long id) throws ResourceNotFoundException{
+        bookingService.deleteSBookingById(id);
+        return "success-delete";
     }
 }
