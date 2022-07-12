e920de2ef65a0b0d64fb9d0e34f5c77f35cb46c1
Harist Islami <islamiharist1998@gmail.com>
2022-07-12 12:59:09 +0700

    Add Jasper Report

---
+++ src/main/java/com/teamc/bioskop/Controller/MVC/MVCPrintController.java
@@ -0,0 +1,30 @@
+package com.teamc.bioskop.Controller.MVC;
+
+import com.teamc.bioskop.Service.ReportPDFService;
+import lombok.AllArgsConstructor;
+import net.sf.jasperreports.engine.JasperExportManager;
+import net.sf.jasperreports.engine.JasperPrint;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.servlet.http.HttpServletResponse;
+
+@Controller
+@AllArgsConstructor
+public class MVCPrintController {
+
+    private ReportPDFService reportPDFService;
+
+    private HttpServletResponse response;
+
+    @GetMapping("/print")
+    public void printReport() throws Exception {
+        response.setContentType("application/pdf");
+        response.setHeader("Content-Disposition", "attachment; filename=\"films_list.pdf\"");
+        JasperPrint jasperPrint = this.reportPDFService.generateJasperPrint();
+        JasperExportManager.exportReportToPdfStream(jasperPrint, response.getOutputStream());
+        response.getOutputStream().flush();
+        response.getOutputStream().close();
+
+    }
+}
--- src/main/java/com/teamc/bioskop/Controller/mvc/MVCFilmController.java
+++ src/main/java/com/teamc/bioskop/Controller/mvc/MVCFilmController.java
@@ -1,17 +1,26 @@
 package com.teamc.bioskop.Controller.MVC;
 
+import com.teamc.bioskop.Model.Attachment;
 import com.teamc.bioskop.Model.Booking;
 import com.teamc.bioskop.Model.Films;
 import com.teamc.bioskop.Model.StatusFilms;
+import com.teamc.bioskop.Response.AttachmentResponse;
 import com.teamc.bioskop.Service.AttachmentService;
 import com.teamc.bioskop.Service.FilmsService;
+import com.teamc.bioskop.Service.ReportPDFService;
 import com.teamc.bioskop.Service.SeatsService;
 import lombok.AllArgsConstructor;
+import net.sf.jasperreports.engine.JasperExportManager;
+import net.sf.jasperreports.engine.JasperPrint;
 import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import javax.servlet.http.HttpServletResponse;
 import java.util.List;
 
 @Controller
@@ -66,13 +75,23 @@ public class MVCFilmController {
         Films films = new Films();
         model.addAttribute("attachment", this.attachmentService.findAllAttachment());
         model.addAttribute("film", films);
+
         return "add-film";
     }
 
     @PostMapping("/management/tambah-film")
     public String submitFilm(@ModelAttribute("film") Films films) {
+
         this.filmsService.createFilm(films);
         return "redirect:/management/films";
+    }
+
+    @PostMapping("/upload-image")
+    public String uploadFile(@RequestParam("file") MultipartFile file, RedirectAttributes redirectAttributes) throws Exception {
+
+        this.attachmentService.saveAttachment(file);
+        redirectAttributes.addFlashAttribute("success", "Success Added Image");
+        return "redirect:/management/add-film";
     }
 
     @GetMapping("/management/edit-film/{id}")
@@ -101,5 +120,6 @@ public class MVCFilmController {
     public String showForm(){
         return "add-images";
     }
+
 
 }
